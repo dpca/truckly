a797d6dbfb9d3895a6cc65a6d7023c298dbbb612
Dan Cahoon <dpcahoon@gmail.com>
2015-11-11 10:24:15 -0500

    Style improvements, es6 const & let, and jsdoc comments

--- app.js
+++ app.js
@@ -1,29 +1,47 @@
 "use strict";
 
 require("dotenv").load();
-var _ = require("lodash");
-var request = require("request");
-var Twit = require("twit");
+const _ = require("lodash");
+const request = require("request");
+const Twit = require("twit");
 
-_.each(["TWITTER_CONSUMER_KEY",
-        "TWITTER_CONSUMER_SECRET",
-        "TWITTER_ACCESS_TOKEN",
-        "TWITTER_ACCESS_TOKEN_SECRET",
-        "SLACK_HOOK_URL"], function(v) {
+_.each([
+  "TWITTER_CONSUMER_KEY",
+  "TWITTER_CONSUMER_SECRET",
+  "TWITTER_ACCESS_TOKEN",
+  "TWITTER_ACCESS_TOKEN_SECRET",
+  "SLACK_HOOK_URL"
+], function(v) {
   if (!process.env[v]) {
     throw new Error(v + " not defined, please set it in .env");
   }
 });
 
+/**
+ * Returns a string day of the week given a day
+ * @param {Integer} date
+ * @return {String} Day of the week
+ */
 function dayOfWeek(date) {
   return ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][date.getDay()];
 }
 
+/**
+ * Returns whether or not the tweet should be posted to slack
+ * @param {Object} truck
+ * @param {Object} tweet
+ * @return {Boolean} True if tweet should be sent to slack
+ */
 function shouldSendTweet(truck, tweet) {
-  var time = new Date(tweet.created_at);
+  let time = new Date(tweet.created_at);
   return _.includes(truck.days, dayOfWeek(time)) && time.getHours() < 14;
 }
 
+/**
+ * Returns a nicely formatted slack attachment to post
+ * @param {Object} tweet
+ * @return {Object} Slack attachment
+ */
 function constructSlackAttachment(tweet) {
   return {
     fallback: "@" + tweet.user.screen_name + ": " + tweet.text,
@@ -35,10 +53,19 @@ function constructSlackAttachment(tweet) {
   };
 }
 
+/**
+ * Returns the message to send to slack
+ * @param {Object} tweet
+ * @return {Object} Slack message body
+ */
 function constructSlackBody(tweet) {
   return { attachments: [ constructSlackAttachment(tweet) ] };
 }
 
+/**
+ * Sends the given tweet to slack
+ * @param {Object} tweet
+ */
 function messageSlack(tweet) {
   request.post({
     url: process.env.SLACK_HOOK_URL,
@@ -48,20 +75,20 @@ function messageSlack(tweet) {
   });
 }
 
-var trucks = require("./config/trucks.json");
+const trucks = require("./config/trucks.json");
 
-var T = new Twit({
+let T = new Twit({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token: process.env.TWITTER_ACCESS_TOKEN,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-var stream = T.stream("statuses/filter", {
+let stream = T.stream("statuses/filter", {
   follow: _.map(trucks, function(truck) { return truck.id; })
 });
 stream.on("tweet", function(tweet) {
-  var thisTruck = _.find(trucks, { id: tweet.user.id });
+  let thisTruck = _.find(trucks, { id: tweet.user.id });
   if (!thisTruck) { return; }
   if (shouldSendTweet(thisTruck, tweet)) {
     messageSlack(tweet);
